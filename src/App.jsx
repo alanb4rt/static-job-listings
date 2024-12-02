c24492fe3b0a2ed3ffbca5b5c59ab40123ec1b87
alanb4rt <al4n.b4rt@gmail.com>
2024-12-02 11:13:15 +0100

    change position of FilterBar

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ export default function App() {
   return (
     <>
       <Header />
-      <FilterBar />
+      <div className="relative z-10 -mt-12">
+        <FilterBar />
+      </div>
       <main className="max-w-screen-lg mx-auto py-4">
         <JobList />
       </main>
