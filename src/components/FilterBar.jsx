86925c296530badb447fccc893350d9b658d3a74
alanb4rt <al4n.b4rt@gmail.com>
2024-12-06 13:05:39 +0100

    add min height to FilterBar

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -8,7 +8,7 @@ export default function FilterBar({ filters, setFilters }) {
   };
 
   return (
-    <div className="flex items-center justify-between gap-8 mb-12 bg-white px-8 py-4 rounded shadow-lg">
+    <div className="min-h-16 flex items-center justify-between gap-8 mb-12 bg-white px-8 py-4 rounded shadow-lg">
       <ul className="flex items-center gap-4 md:gap-8 flex-wrap">
         {filters.map((filter) => (
           <div
