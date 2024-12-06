65d46c2bf539cc2d5a992f44c7d6b3575912efc8
alanb4rt <al4n.b4rt@gmail.com>
2024-12-06 12:58:14 +0100

    fix padding of Badge and JobSkill

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,7 +1,7 @@
 export default function Badge({ children, color }) {
   return (
     <div
-      className="text-sm font-bold uppercase rounded-full text-white px-2"
+      className="text-sm font-bold uppercase rounded-full text-white px-2 pt-1"
       style={{ background: color }}
     >
       {children}
--- src/components/JobSkill.jsx
+++ src/components/JobSkill.jsx
@@ -1,7 +1,7 @@
 export default function JobSkill({ children, className, onClick }) {
   return (
     <li
-      className={`bg-[var(--color-bg)] text-[var(--color-primary)] font-bold p-2 leading-none rounded ${
+      className={`bg-[var(--color-bg)] text-[var(--color-primary)] font-bold p-2 pb-1 leading-none rounded ${
         className || ""
       }`}
       onClick={onClick}
